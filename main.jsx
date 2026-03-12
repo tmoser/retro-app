8b13fa1adff7f21c648d897acc0ce36f9a455ad9
tmoser <tmoser@oreilly.com>
2026-03-11 22:20:47 -0400

    Create main.jsx that sets up React application entry point

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './retro-app';
+
+ReactDOM.render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n  document.getElementById('root')\n);
